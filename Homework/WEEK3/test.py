3cdf9be3d4326ef4c8c894b83c5091c3b9a09226
Yuqiao_Li <yl13307@nyu.edu>
2025-04-29 09:29:38 -0400

    WEEK 8 HW

---
+++ Homework/WEEK3/test.py
@@ -0,0 +1,56 @@
+#/station-route-map
+{
+  "R01": ["1", "2", "3"],
+  "R03": ["1", "2", "3"],
+}
+#/stations/A33S/routes
+{
+  "routes": [
+    {
+      "color": "2850AD",
+      "id": "A",
+      "long_name": "8 Avenue Express",
+      "short_name": "A",
+      "text_color": "FFFFFF"
+    },
+    {
+      "color": "2850AD",
+      "id": "C",
+      "long_name": "8 Avenue Local",
+      "short_name": "C",
+      "text_color": "FFFFFF"
+    },
+   ...
+  ],
+  "station_id": "A33S"
+}
+#/api/routes/<route_id>/stops
+{
+  "route_id": "1",
+  "stops": [
+    {
+      "id": "101N",
+      "lat": 40.889248,
+      "lng": -73.898583,
+      "name": "Van Cortlandt Park-242 St"
+    },
+    {
+      "id": "101S",
+      "lat": 40.889248,
+      "lng": -73.898583,
+      "name": "Van Cortlandt Park-242 St"
+    },
+    {
+      "id": "103N",
+      "lat": 40.884667,
+      "lng": -73.90087,
+      "name": "238 St"
+    },
+    {
+      "id": "103S",
+      "lat": 40.884667,
+      "lng": -73.90087,
+      "name": "238 St"
+    },
+    ...]
+    }
---
+++ Homework/WEEK8/Maximum Subarray.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        current_max = global_max = nums[0]
+        
+        for i in range(1, len(nums)):
+            
+            current_max = max(nums[i], current_max + nums[i])
+            
+            global_max = max(global_max, current_max)
+            
+        return global_max
